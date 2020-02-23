09b4e12f72a467e6e3eceac873348ab10038bb2b
Manuel Ramirez <manuelramirez1411@gmail.com>
2020-02-23 18:51:51 -0500

    chage order

--- schema.sql
+++ schema.sql
@@ -4,29 +4,33 @@ CREATE DATABASE employee_trackerDB;
 
 USE employee_trackerDB;
 
-CREATE TABLE employee (
-id INT AUTO_INCREMENT NOT NULL,
-first_name varchar (30),
-last_name varchar (30),
-role_id INT,
-manager_id INT NULL,
-PRIMARY KEY(id)
-);
- 
+ CREATE TABLE department (
+ id INT AUTO_INCREMENT NOT NULL,
+ name VARCHAR (30),
+ PRIMARY KEY (id)
+  
+ );
+
  CREATE TABLE role (
  id INT AUTO_INCREMENT NOT NULL,
  tittle VARCHAR (30),
  salary DECIMAL (12,4),
  department_id INT,
  PRIMARY KEY (id),
- FOREIGN KEY (id) REFERENCES employee (id)
+ FOREIGN KEY (id) REFERENCES department (id)
  );
+
+ 
+CREATE TABLE employee (
+id INT AUTO_INCREMENT NOT NULL,
+first_name varchar (30),
+last_name varchar (30),
+role_id INT,
+manager_id INT NULL,
+PRIMARY KEY(id),
+FOREIGN KEY (id) REFERENCES role (id),
+FOREIGN KEY (id) REFERENCES employee (id)
+);
+ 
  
 
- CREATE TABLE department (
- id INT AUTO_INCREMENT NOT NULL,
- name VARCHAR (30),
- PRIMARY KEY (id)
-  FOREIGN KEY (id) REFERENCES role (id)
- );
- 
