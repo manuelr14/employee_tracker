ac62cae52b0399de3955ba1d624308353195b2b3
Manuel Ramirez <manuelramirez1411@gmail.com>
2020-02-24 16:10:22 -0500

    formated

--- employee_tracker.js
+++ employee_tracker.js
@@ -2,84 +2,84 @@ const mysql = require("mysql");
 const inquirer = require("inquirer");
 
 const connection = mysql.createConnection({
-  host: "localhost",
+    host: "localhost",
 
-  // Your port; if not 3306
-  port: 3306,
+    // Your port; if not 3306
+    port: 3306,
 
-  // Your username
-  user: "root",
+    // Your username
+    user: "root",
 
-  // Your password
-  password: "18877MSca",
-  database: "employee_trackerDB"
+    // Your password
+    password: "18877MSca",
+    database: "employee_trackerDB"
 });
 
 connection.connect(function (err) {
-  if (err) throw err;
-  //Make sure we're calling our runSearch function ONLY AFTER our connection to the database was successfully established
-  runSearch();
+    if (err) throw err;
+    //Make sure we're calling our runSearch function ONLY AFTER our connection to the database was successfully established
+    runSearch();
 });
 
 function runSearch() {
-  //Run an inquirer prompt to ask for the user's desired action
-  inquirer
-    .prompt({
-      name: "action",
-      type: "rawlist",
-      message: "What would you like to do?",
-      choices: [
-        "View all employees",
-        "View all employees by department",
-        "View all employees by Manager",
-        "Add employee",
-        "Remove employee",
-        "Update employee role",
-        "Update employee manager"
-      ]
-    })
-    .then(answer => {
-      //Based on the selected action, call one of our functions to query the database
-      switch (answer.action) {
-        case "View all employees":
-          allEmployees();
-          break;
+    //Run an inquirer prompt to ask for the user's desired action
+    inquirer
+        .prompt({
+            name: "action",
+            type: "rawlist",
+            message: "What would you like to do?",
+            choices: [
+                "View all employees",
+                "View all employees by department",
+                "View all employees by Manager",
+                "Add employee",
+                "Remove employee",
+                "Update employee role",
+                "Update employee manager"
+            ]
+        })
+        .then(answer => {
+            //Based on the selected action, call one of our functions to query the database
+            switch (answer.action) {
+                case "View all employees":
+                    allEmployees();
+                    break;
 
-        case "View all employees by department":
-          allEmployees_byDep();
-          break;
+                case "View all employees by department":
+                    allEmployees_byDep();
+                    break;
 
-        case "View all employees by Manager":
-          allEmployees_byManag();
-          break;
+                case "View all employees by Manager":
+                    allEmployees_byManag();
+                    break;
 
-        case "Add employee":
-          addEmployee();
-          break;
+                case "Add employee":
+                    addEmployee();
+                    break;
 
-        case "Update employee role":
-          updateRole();
-          break;
+                case "Update employee role":
+                    updateRole();
+                    break;
 
-        case "Update employee manager":
-          updateManager();
-          break;
-      }
-    });
+                case "Update employee manager":
+                    updateManager();
+                    break;
+            }
+        });
 }
 
 function allEmployees() {
 
-  connection.query("SELECT employee_id, first_name, last_name, role.tittle, role.salary FROM employee JOIN role ON employee.role_id = role.role_id",
-    (err, results) => {
-      if (err) throw err;
+    connection.query("SELECT employee_id, first_name, last_name, role.tittle, role.salary FROM employee JOIN role ON employee.role_id = role.role_id",
+        (err, results) => {
+            if (err) throw err;
 
-      results.forEach(element => {
+            results.forEach(element => {
 
-        console.log('employeerID:' + element.employee_id + ' || name: ' + element.first_name + ' || last name: ' + element.last_name + ' || Role: ' + element.tittle + ' || Salary: ' + element.salary)
+                console.log('employeerID:' + element.employee_id + ' || name: ' + element.first_name + ' || last name: ' + element.last_name + ' || Role: ' + element.tittle + ' || Salary: ' + element.salary)
 
-      });
+            });
 
-    }
-  )
+        }
+    )
 };
