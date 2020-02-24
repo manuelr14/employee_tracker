254d536a529005114617ff392b3c9dfdbdba8c51
Manuel Ramirez <manuelramirez1411@gmail.com>
2020-02-23 19:00:03 -0500

    selfreference key added

--- schema.sql
+++ schema.sql
@@ -5,32 +5,31 @@ CREATE DATABASE employee_trackerDB;
 USE employee_trackerDB;
 
  CREATE TABLE department (
- id INT AUTO_INCREMENT NOT NULL,
+ department_id INT AUTO_INCREMENT NOT NULL,
  name VARCHAR (30),
- PRIMARY KEY (id)
+ PRIMARY KEY (department_id)
   
  );
 
  CREATE TABLE role (
- id INT AUTO_INCREMENT NOT NULL,
+ role_id INT AUTO_INCREMENT NOT NULL,
  tittle VARCHAR (30),
  salary DECIMAL (12,4),
  department_id INT,
- PRIMARY KEY (id),
- FOREIGN KEY (id) REFERENCES department (id)
+ PRIMARY KEY (role_id),
+ FOREIGN KEY (role_id) REFERENCES department (department_id)
  );
-
+ 
  
 CREATE TABLE employee (
-id INT AUTO_INCREMENT NOT NULL,
+employee_id INT AUTO_INCREMENT NOT NULL,
 first_name varchar (30),
 last_name varchar (30),
 role_id INT,
 manager_id INT NULL,
-PRIMARY KEY(id),
-FOREIGN KEY (id) REFERENCES role (id),
-FOREIGN KEY (id) REFERENCES employee (id)
+PRIMARY KEY(employee_id),
+FOREIGN KEY (employee_id) REFERENCES role (role_id),
+FOREIGN KEY (manager_id) REFERENCES employee (employee_id)
 );
- 
- 
+
 
